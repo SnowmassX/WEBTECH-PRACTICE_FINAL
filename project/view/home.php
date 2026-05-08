95b11ebcc588e85ad00fae970ca332cbd1a28502
SnowmassX <snowmass1056@gmail.com>
2026-05-08 23:49:09 +0600

    initial commit for project

---
+++ project/view/home.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <section>
+            <h2>Amar Bazar</h2>
+        </section>
+        <section>
+            <form action="" method="get">
+                <input type="text" name="search">
+            </form>
+        </section>
+        <nav>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href=".php">Cart</a>
+        </nav>
+    </header>
+</body>
+</html>
